d72e8324cad9a7c60669d746b82cf88598a228fc
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-05-17 17:05:51 -0400

    Time: 12 ms (93.63%), Space: 10.6 MB (15.33%) - LeetHub

---
+++ number-of-islands/number-of-islands.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.empty() || grid[0].empty()) return 0;
+        int m=grid.size(),n=grid[0].size(),res=0;
+        vector<vector<bool>> visited(m,vector<bool>(n));
+        vector<int> corX={-1,0,1,0},corY={0,1,0,-1};
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='0'||visited[i][j]==true) continue;
+                ++res;
+                queue<int> q{{i*n+j}};
+                while(!q.empty()){
+                    int t=q.front();q.pop();
+                    for(int k=0;k<4;k++){
+                        int x=t/n+corX[k],y=t%n+corY[k];
+                        if(x<0 || y<0 || x>=m || y>=n || grid[x][y]=='0' || visited[x][y])
+                            continue;
+                        visited[x][y]=true;
+                        q.push(x*n+y);
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
+};
