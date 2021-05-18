991d48928cfa8ceff1a4e83bf57a1a42ce338a43
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-05-18 00:34:57 -0400

    Time: 24 ms (37.72%), Space: 14.4 MB (31.27%) - LeetHub

--- graph-valid-tree/graph-valid-tree.cpp
+++ graph-valid-tree/graph-valid-tree.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    //DFS
-    bool helper(vector<vector<int>>& graph,vector<bool>& visited,int cur,int pre){
+    bool helper(vector<vector<int>>& graph, vector<bool>& visited,int cur,int pre){
         if(visited[cur]==true) return false;
         visited[cur]=true;
-        for(auto j:graph[cur]){
-            if(j!=pre){
-                if(helper(graph,visited,j,cur)==false) return false; 
+        for(int i=0;i<graph[cur].size();i++){
+            if(graph[cur][i]!=pre){
+                if(!helper(graph,visited,graph[cur][i],cur))
+                    return false;
             }
         }
         return true;
@@ -14,17 +14,15 @@ public:
     
     bool validTree(int n, vector<vector<int>>& edges) {
         vector<vector<int>> graph(n,vector<int>());
-        vector<bool> visited(n,false);
         for(auto i:edges){
             graph[i[0]].push_back(i[1]);
             graph[i[1]].push_back(i[0]);
         }
-        
-        if(!helper(graph,visited,0,-1)) return false;
-        
-        //有一个单独的点，根本没被遍历到，不连通，不是tree
-        for(auto i:visited){
-            if(i==false) return false;
+        vector<bool> visited(n,false);
+        if(!helper(graph,visited,0,-1))
+            return false;
+        for(int i=0;i<visited.size();i++){
+            if(visited[i]==false) return false;
         }
         return true;
     }
