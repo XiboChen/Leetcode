f3589ae71c6c404685e267ae72f408206cb1a4a1
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-06-20 18:25:47 -0400

    Time: 4 ms (50.63%), Space: 8.8 MB (31.72%) - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> res;
+        for(auto num:nums){
+            if(num!=val)
+                res.push_back(num);
+        }
+        for(int i=0;i<res.size();i++){
+            nums[i]=res[i];
+        }
+        return res.size();
+    }
+};
