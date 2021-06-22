97421403ba8c43368e547b59c0fbfef6f1ebeba6
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-06-22 15:18:15 -0400

    Time: 0 ms (100.00%), Space: 7 MB (30.25%) - LeetHub

---
+++ implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,49 @@
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) { //保证push时是反过来存，这样最上面的是第一个push的
+        stack<int> tmp;
+        while(!s.empty()){
+            tmp.push(s.top());
+            s.pop();
+        }
+        s.push(x);
+        while(!tmp.empty()){
+            s.push(tmp.top());
+            tmp.pop();
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int val=s.top();
+        s.pop();
+        return val;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        return s.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return s.empty();
+    }
+private:
+    stack<int> s;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
