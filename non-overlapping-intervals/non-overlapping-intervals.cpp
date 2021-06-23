415a78ec37b7d4ae697c0c8272db8effaefe6fd1
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-06-23 14:45:16 -0400

    Time: 16 ms (65.25%), Space: 10.3 MB (58.45%) - LeetHub

---
+++ non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int res=0,n=intervals.size(),last=0;
+        sort(intervals.begin(),intervals.end());
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<intervals[last][1]){
+                res++;
+                if(intervals[i][1]<intervals[last][1]) last=i; //保留end值较小的
+            }
+            else last=i;
+        }
+        return res;
+    }
+};
