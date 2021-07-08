38fa5aad02a10b0293e7dea153f1b677ddb8ca20
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-07-08 18:10:47 -0400

    Time: 12 ms (85.87%), Space: 10.2 MB (62.04%) - LeetHub

---
+++ lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -0,0 +1,23 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+        Node* a=p,*b=q;
+        while(a!=b){
+            a=(a==NULL)?q:a->parent;
+            b=(b==NULL)?p:b->parent;
+        }
+        return a;
+    }
+    /*给了parent所以可以从p,q往回找*/
+};
