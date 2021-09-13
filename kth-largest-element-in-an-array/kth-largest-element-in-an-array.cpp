3118cadcc963543f116ac5a585369cf5c320970c
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-13 11:37:54 -0400

    Time: 4 ms (97.61%), Space: 10.2 MB (18.06%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> q;
-        int res=0;
+        priority_queue<int,vector<int>,greater<int>> q;
         for(auto num:nums){
             q.push(num);
+            if(q.size()>k){
+                q.pop();
+            }
         }
-        for(int i=0;i<k-1;i++){
-            q.pop();
-        }
-        res=q.top();
-        return res;
+        
+        return q.top();
     }
 };
