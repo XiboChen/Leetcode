ca1e9903958f2977a86a1ef04d923e86a7b68cb6
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-13 17:24:14 -0400

    Time: 0 ms (100.00%), Space: 7 MB (29.84%) - LeetHub

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -1,61 +1,23 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m=matrix.size(),n=matrix[0].size();
         vector<int> res;
+        int m=matrix.size(),n=matrix[0].size();
         int up=0,down=m-1,left=0,right=n-1;
         while(1){
-            for(int j=left;j<=right;j++)  //横向最上
-                res.push_back(matrix[up][j]);
+            for(int i=left;i<=right;++i)
+                res.push_back(matrix[up][i]);
             if(++up>down) break;
-            for(int i=up;i<=down;i++)  //竖向最右
+            for(int i=up;i<=down;++i)
                 res.push_back(matrix[i][right]);
             if(--right<left) break;
-            for(int j=right;j>=left;--j)
-                res.push_back(matrix[down][j]);
+            for(int i=right;i>=left;--i)
+                res.push_back(matrix[down][i]);
             if(--down<up) break;
-            for(int i=down;i>=up;--i) 
+            for(int i=down;i>=up;--i)
                 res.push_back(matrix[i][left]);
             if(++left>right) break;
         }
         return res;
-        
-        // int t=0;
-        // int r=matrix.size();
-        // int b=matrix[0].size();
-        // int l=0;
-        // int total=r*b;
-        // r=r-1;
-        // b=b-1;
-        // int d=0;
-        // int x=0,y=0;
-        // vector<int> res;
-        // while(res.size()<total-1){
-        //     if(d==0){
-        //         while(x<b)
-        //             res.push_back(matrix[y][x++]);
-        //         t++;
-        //     }
-        //     else if(d==1){
-        //         while(y<r)
-        //             res.push_back(matrix[y++][x]);
-        //         r--;
-        //     }
-        //     else if(d==2){
-        //         while(x>l)
-        //             res.push_back(matrix[y][x--]);
-        //         b--;
-        //     }
-        //     else if(d==3){
-        //         while(y>t)
-        //             res.push_back(matrix[y--][x]);
-        //         l++;
-        //     }
-        //     d=(d+1)%4;
-        // }
-        // if(res.size()!=total)
-        //     res.push_back(matrix[y][x]);
-        // return res;
     }
 };
-
