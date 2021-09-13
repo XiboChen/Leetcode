1e73516cf121ff206838ace74cb3b9b751fedf5c
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-13 13:43:32 -0400

    Time: 7 ms (21.77%), Space: 9 MB (99.64%) - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res="";
+        for(int j=0;j<strs[0].size();++j){
+            char c=strs[0][j];
+            for(int i=0;i<strs.size();++i){
+                if(j>strs[i].size()||strs[i][j]!=c) return res;
+            }
+            res+=c;
+        }
+        return res;
+    }
+};
