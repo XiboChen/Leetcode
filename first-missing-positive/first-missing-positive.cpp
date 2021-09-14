5b76ace67bcb24ab189e6240c142b90837017470
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-14 15:44:02 -0400

    Time: 541 ms (5.15%), Space: 120.7 MB (14.00%) - LeetHub

--- first-missing-positive/first-missing-positive.cpp
+++ first-missing-positive/first-missing-positive.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int>s(nums.begin(),nums.end());
+        unordered_set<int> s(nums.begin(),nums.end());
         int res=1,n=nums.size();
         while(res<=n){
-            if(!s.count(res)) return res;
-            ++res;
+            if(s.count(res)) ++res;
+            else break;
         }
         return res;
     }
