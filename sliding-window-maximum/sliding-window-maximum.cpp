6ec16c8e3d5d3053ca89aeef8b0ac1abe1984ab2
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-16 12:56:43 -0400

    Time: 929 ms (5.01%), Space: 206.8 MB (8.80%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.cpp
+++ sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> res;
-        priority_queue<pair<int,int>> q;
-        for(int i=0;i<nums.size();i++){
-            while(!q.empty()&&q.top().second<=i-k)
-                    q.pop();
-            q.push(make_pair(nums[i],i));
-            if(i>=k-1) res.push_back(q.top().first);
+        multiset<int> s;
+        for(int i=0;i<nums.size();++i){
+            if(i>=k) s.erase(s.find(nums[i-k]));
+            s.insert(nums[i]);
+            if(i>=k-1) res.push_back(*s.rbegin());
         }
         return res;
     }
