d02f8b1533d824b9d88b3efef2b6ce400b11bd99
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-16 11:26:22 -0400

    Time: 0 ms (100.00%), Space: 6.6 MB (34.27%) - LeetHub

--- decode-string/decode-string.cpp
+++ decode-string/decode-string.cpp
@@ -5,26 +5,19 @@ public:
         stack<int> s_num;
         stack<string> s_str;
         int cnt=0;
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i<s.size();++i){
             if(isdigit(s[i])){
-                cnt=10*cnt+s[i]-'0';
-            }
-            else if(s[i]=='['){
+                cnt=cnt*10+(s[i]-'0');
+            }else if(s[i]=='['){
                 s_num.push(cnt);
                 s_str.push(t);
-                cnt=0;
-                t.clear();
-            }
-            else if(s[i]==']'){
-                int k=s_num.top();s_num.pop();
-                string tmp="";
-                for(int j=0;j<k;j++){
-                    tmp+=t;
-                }
-                t=s_str.top()+tmp;
-                s_str.pop();
-            }
-            else{
+                cnt=0;t.clear();
+            }else if(s[i]==']'){
+                auto n=s_num.top();s_num.pop();
+                for(int j=0;j<n;++j)
+                    s_str.top()+=t;
+                t=s_str.top();s_str.pop();
+            }else{
                 t+=s[i];
             }
         }
