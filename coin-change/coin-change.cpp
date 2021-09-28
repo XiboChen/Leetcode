e74daa8ffcea468644d5a90b313af879dd15bbc2
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-28 14:50:21 -0400

    Time: 96 ms (43.01%), Space: 14.5 MB (47.94%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -3,13 +3,13 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1,amount+1);
         dp[0]=0;
-        for(int i=1;i<amount+1;i++){
-            for(int j=0;j<coins.size();j++){
+        for(int i=1;i<=amount;++i){
+            for(int j=0;j<coins.size();++j){
                 if(coins[j]<=i)
                     dp[i]=min(dp[i],dp[i-coins[j]]+1);
             }
         }
+        //for(auto i:dp) cout<<i<<" ";
         return dp[amount]==amount+1?-1:dp[amount];
     }
 };
-//dp[i]钱数为i时的最小硬币数
