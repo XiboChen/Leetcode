1a71b1e14f214616d3096e7ef4266c32df6c6be2
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-29 16:01:19 -0400

    Time: 8 ms (61.09%), Space: 8.8 MB (29.75%) - LeetHub

--- integer-to-roman/integer-to-roman.cpp
+++ integer-to-roman/integer-to-roman.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string intToRoman(int num) {
         string res="";
-        vector<int> val{1000,900,500,400,100,90,50,40,10,9,5,4,1};
-    vector<string> str{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        for(int i=0;i<val.size();i++){
-            while(num>=val[i]){
-                num-=val[i];
+        vector<int> dig{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string> str{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        for(int i=0;i<dig.size();++i){
+            while(num>=dig[i]){
                 res+=str[i];
+                num-=dig[i];
             }
         }
         return res;
