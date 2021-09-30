0366d4ded96c960b793350fca2e63d4cb8a0874a
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-09-30 15:34:52 -0400

    Time: 16 ms (61.79%), Space: 11.6 MB (28.71%) - LeetHub

--- reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -11,31 +11,26 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *dummy=new ListNode(-1),*pre=dummy,*cur=head;
+        ListNode* dummy=new ListNode(0);
         dummy->next=head;
-        int cnt=1;
-        while(cur){
-            if(cnt%k==0){
-                ListNode *next=cur->next;
-                pre=reverse(pre,cur->next);
-                cur=pre->next;
+        int len=0;
+        while(head){
+            len++;
+            head=head->next;
+        }
+        ListNode* pre=dummy;
+        for(int l=0;l+k<=len;l+=k){
+            ListNode* cur=pre->next;
+            ListNode* nxt=cur->next;
+            for(int i=1;i<k;++i){
+                cur->next=nxt->next;
+                nxt->next=pre->next;
+                pre->next=nxt;
+                nxt=cur->next;
             }
-            else cur=cur->next;
-            cnt++;
+            pre=cur;
         }
+        
         return dummy->next;
-    }
-    //reverse function
-    //传进来的next一直不变，方便最后指向它，pre也不变
-    ListNode* reverse(ListNode *pre,ListNode *next){
-        ListNode *last=pre->next,*cur=last->next;//设last，会reverse成最后一个
-        while(cur!=next){
-            //转换完成后，last最后一个，next是next，而cur等于lastnext，正好停止
-            last->next=cur->next;//其实是两两翻转
-            cur->next=pre->next;
-            pre->next=cur;
-            cur=last->next;
-        }
-        return last; //此时last对应的正好是传入前的pre
     }
 };
