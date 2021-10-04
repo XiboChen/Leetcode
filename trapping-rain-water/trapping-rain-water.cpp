79d4379b14096f4bdda228ce2a6ed6801239a153
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-04 16:02:43 -0400

    Time: 20 ms (12.22%), Space: 16.1 MB (30.40%) - LeetHub

--- trapping-rain-water/trapping-rain-water.cpp
+++ trapping-rain-water/trapping-rain-water.cpp
@@ -1,65 +1,24 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size(),res=0,mx=0;
-        vector<int> dp(n,0);
-        vector<int> dp2(n,0);
+        int res=0,n=height.size(),max_=0;
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        for(int i=1;i<n;++i){
+            max_=max(max_,height[i-1]);
+            left[i]=max(left[i-1],max_);
+        }
+        max_=0;
+        for(int i=n-2;i>=0;--i){
+            max_=max(max_,height[i+1]);
+            right[i]=max(right[i+1],max_);
+        }
         for(int i=0;i<n;++i){
-            dp[i]=mx;
-            mx=max(mx,height[i]);
-        }
-        //for(auto i:dp) cout<<i<<" ";
-        mx=0;
-        for(int i=n-1;i>=0;--i){
-            dp2[i]=mx;
-            mx=max(mx,height[i]);
-           
-        }
-        // cout<<endl;
-        // for(auto i:dp2) cout<<i<<" ";
-        // cout<<endl;
-        for(int i=0;i<dp.size();++i){
-            int sm=min(dp[i],dp2[i]);
-            //cout<<sm<<" ";
-            if(sm>height[i]){
-                res+=sm-height[i];
+            int t=min(left[i],right[i]);
+            if(t>height[i]){
+                res+=t-height[i];
             }
         }
         return res;
-        
-        
-        
-        
-//         stack<int> st; //压入坐标
-//         int i=0,n=height.size(),res=0;
-//         while(i<n){
-//             if(st.empty()||height[i]<=height[st.top()]) st.push(i++);
-//             else{
-//                 int t=st.top();st.pop();
-//                 if(st.empty()) continue;
-      
-//                 res+=(min(height[i],height[st.top()])-height[t])*(i-st.top()-1);
-                
-//             }
-//         }
-//         return res;
-        
-        
-        // int n=height.size(),res=0,mx=0;
-        // vector<int> dp(n,0);
-        // for(int i=0;i<n;++i){
-        //     dp[i]=mx;
-        //     mx=max(mx,height[i]);
-        // }
-        //  for(auto i:dp) cout<<i<<"  ";cout<<endl;
-        // mx=0;
-        // //确定右边最大的时候，已经确定好了左右两边较低的那个高作为短板
-        // for(int i=n-1;i>=0;--i){
-        //     dp[i]=min(dp[i],mx);
-        //     mx=max(mx,height[i]);
-        //     if(dp[i]>height[i]) res+=dp[i]-height[i];
-        // }
-        // for(auto i:dp) cout<<i<<"  ";
-        // return res;
     }
 };
