94231db15773051e28c994c5580d9348f808f4e6
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-04 14:36:35 -0400

    Time: 0 ms (100.00%), Space: 6.3 MB (19.97%) - LeetHub

--- robot-bounded-in-circle/robot-bounded-in-circle.cpp
+++ robot-bounded-in-circle/robot-bounded-in-circle.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool isRobotBounded(string instructions) {
-        int idx=0; //0-北，1-东，2-南，3-西
-        vector<vector<int>> dirs{{0,1},{1,0},{0,-1},{-1,0}};
-        int x=0,y=0;
-        for(auto i:instructions){
-            if(i=='G') {
-                x+=dirs[idx][0];
-                y+=dirs[idx][1];
+        int d=0; //0-N,1-E,2-S,3-W
+        vector<vector<int>> dir{{0,1},{1,0},{0,-1},{-1,0}};
+        vector<int> cur{0,0};
+        for(auto ins:instructions){
+            if(ins=='G'){
+                cur[0]+=dir[d][0];
+                cur[1]+=dir[d][1];
             }
-            else if(i=='L'){
-                idx=(idx+4-1)%4;
+            else if(ins=='L'){
+                d=(d+4-1)%4;
             }
-            else
-                idx=(idx+1)%4;
+            else if(ins=='R'){
+                d=(d+1)%4;
+            }
         }
-        return x==0&&y==0||idx>0;
+        return cur[0]==0&&cur[1]==0?true:(d==0?false:true);
     }
 };
