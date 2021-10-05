64665eff05e4eb9e406cae5452cd1fb173879221
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-05 17:24:14 -0400

    Time: 0 ms (100.00%), Space: 9.5 MB (54.21%) - LeetHub

--- verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        vector<char> v(26);
-        for(int i=0;i<26;++i){
-            v[order[i]-'a']=i+'a';
+        unordered_map<char,int> m;
+        for(int i=0;i<order.size();++i){
+            m[order[i]]=i;
         }
         for(int i=0;i<words.size();++i){
             for(int j=0;j<words[i].size();++j){
-                words[i][j]=v[words[i][j]-'a'];
+                words[i][j]=m[words[i][j]];
             }
             if(i>0&&words[i]<words[i-1]) return false;
         }
