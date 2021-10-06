bb8abb656106b71d79a1081f12d195f03cf5b08c
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-06 17:13:09 -0400

    Time: 0 ms (100.00%), Space: 6.5 MB (65.70%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         if(digits.size()==0) return {};
+        vector<vector<char>> let{{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
         vector<string> res;
-        vector<vector<char>> v{{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
-        string t;         
-        dfs(digits,res,v,t,0);
+        dfs(digits,res,let,"",0);
         return res;
     }
     
-    void dfs(string digits,vector<string>& res,vector<vector<char>>& v,string& t, int i){
+    void dfs(string& digits,vector<string>& res,vector<vector<char>>& let,string cur,int i){
         if(i==digits.size()){
-            res.push_back(t);
+            res.push_back(cur);
             return;
         }
-       
-        for(auto k:v[digits[i]-'0']){
-            t+=k;
-            dfs(digits,res,v,t,i+1);
-            t.pop_back();
-        }        
+        for(auto k:let[digits[i]-'0']){
+            cur+=k;
+            dfs(digits,res,let,cur,i+1);
+            cur.pop_back();
+        }
     }
 };
