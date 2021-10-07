4b3a461966ae8dac6405907afbd2f2be496b66f9
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-07 18:30:05 -0400

    Time: 0 ms (100.00%), Space: 15.7 MB (20.20%) - LeetHub

--- generate-parentheses/generate-parentheses.cpp
+++ generate-parentheses/generate-parentheses.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        string cur;
-        dfs(n,res,cur,0,0);
+        dfs(res,"",n,n);
         return res;
     }
     
-    void dfs(int& n, vector<string>& res,string& cur, int l,int r){
-        if(l+r==2*n){
+    void dfs(vector<string>& res,string cur,int l,int r){
+        if(l+r==0){
             res.push_back(cur);
             return;
         }
-        if(r>l) return; 
-        if(l<n){
-            dfs(n,res,cur+='(',l+1,r);
+        if(l>r) return;
+        if(l>0){
+            cur+='(';
+            dfs(res,cur,l-1,r);
             cur.pop_back();
         }
-        if(r<n){
-            dfs(n,res,cur+=')',l,r+1);
+        if(r>0){
+            cur+=')';
+            dfs(res,cur,l,r-1);
             cur.pop_back();
         }
     }
