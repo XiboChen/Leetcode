c5e9dc5f34c2090ce9478e0567c3c5832c7bd961
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-07 15:02:43 -0400

    Time: 0 ms (100.00%), Space: 7.6 MB (84.96%) - LeetHub

--- expressive-words/expressive-words.cpp
+++ expressive-words/expressive-words.cpp
@@ -2,13 +2,22 @@ class Solution {
 public:
     int expressiveWords(string s, vector<string>& words) {
         int res=0,m=s.size(),n=words.size();
-        for(string word:words){
+        for(auto word:words){
             int i=0,j=0;
             while(i<m){
-                if(j<word.size()&&s[i]==word[j]) ++j;
-                else if(i>0&&s[i]==s[i-1]&&i+1<m&&s[i]==s[i+1]) ++i;
-                else if(!(i>1&&s[i]==s[i-1]&&s[i]==s[i-2])) break;
-                ++i;
+                if(j<word.size()&&s[i]==word[j]){
+                    ++i;++j;
+                    continue;
+                } 
+                else if(i>0&&s[i]==s[i-1]&&i+1<m&&s[i]==s[i+1]){
+                    i+=2;
+                    continue;
+                }
+                else if(i>1&&s[i]==s[i-1]&&s[i]==s[i-2]){
+                    ++i;
+                    continue;
+                }
+                else break;
             }
             if(i==m&&j==word.size()) ++res;
         }
