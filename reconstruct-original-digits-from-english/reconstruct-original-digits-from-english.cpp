f8c63256c59c422842d7bbdf34849e1d77b7480b
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-08 13:52:40 -0400

    Time: 12 ms (91.47%), Space: 8.9 MB (62.40%) - LeetHub

--- reconstruct-original-digits-from-english/reconstruct-original-digits-from-english.cpp
+++ reconstruct-original-digits-from-english/reconstruct-original-digits-from-english.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     string originalDigits(string s) {
-        vector<int> nums(10,0);
-        vector<int> counts(128,0);
-        for(auto c:s) counts[c]++;
         string res="";
-        
-        nums[0]=counts['z'];
-        nums[2]=counts['w'];
-        nums[4]=counts['u'];
-        nums[6]=counts['x'];
-        nums[8] = counts['g'];
-        nums[1] = counts['o'] - nums[0] - nums[2] - nums[4];
-        nums[3] = counts['h'] - nums[8];
-        nums[5] = counts['f'] - nums[4];
-        nums[7] = counts['s'] - nums[6];
-        nums[9] = counts['i'] - nums[6] - nums[8] - nums[5];
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = 0; j < nums[i]; ++j) {
-                res += (i + '0');
+        vector<int> count(128,0),nums(10,0);
+        for(auto i:s) count[i]++;
+        nums[0]=count['z'];
+        nums[2]=count['w'];
+        nums[4]=count['u'];
+        nums[6]=count['x'];
+        nums[8]=count['g'];
+        nums[1]=count['o']-nums[0]-nums[2]-nums[4];
+        nums[3]=count['h']-nums[8];
+        nums[5]=count['f']-nums[4];
+        nums[7]=count['s']-nums[6];
+        nums[9]=count['i']-nums[6]-nums[8]-nums[5];
+      
+        for(int i=0;i<nums.size();++i){
+            for(int j=0;j<nums[i];++j){
+                res+=(i+'0');
             }
         }
         return res;
