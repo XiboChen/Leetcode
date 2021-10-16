128d58b105f1755918b93882e5329439eabf95a3
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-16 15:24:36 -0400

    Time: 24 ms (95.76%), Space: 11.6 MB (85.42%) - LeetHub

--- maximal-rectangle/maximal-rectangle.cpp
+++ maximal-rectangle/maximal-rectangle.cpp
@@ -3,23 +3,46 @@ public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int m=matrix.size();
         if(m==0) return 0;
-        int n=matrix[0].size(),res=0;
+        int n=matrix[0].size();
+        
         vector<vector<int>> dp(m,vector<int>(n));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<m;++i){   //得到每一行最长的矩形
+            for(int j=0;j<n;++j){
                 dp[i][j]=(matrix[i][j]=='1')?(j==0?1:dp[i][j-1]+1):0;
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        // for(auto i:dp){
+        //     for(auto j:i) cout<<j<<" ";cout<<endl;
+        // }
+        int res=0;
+        for(int i=0;i<m;++i){  //以每一行作为矩形最上面的边
+            for(int j=0;j<n;++j){  //以每一行的某一列的点作为矩形的左上角
                 int len=INT_MAX;
-                for(int k=i;k<m;k++){
+                for(int k=i;k<m;++k){
                     len=min(len,dp[k][j]);
                     if(len==0) break;
                     res=max(res,len*(k-i+1));
                 }
             }
+            
         }
+        
         return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
