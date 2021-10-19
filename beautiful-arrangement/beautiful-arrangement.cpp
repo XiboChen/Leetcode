1e76f6ae87490a61d27a14379c1c30cc94389c8a
Xibo Chen <59970225+XiboChen@users.noreply.github.com>
2021-10-19 14:02:17 -0400

    Time: 194 ms (26.46%), Space: 6 MB (58.67%) - LeetHub

--- beautiful-arrangement/beautiful-arrangement.cpp
+++ beautiful-arrangement/beautiful-arrangement.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int countArrangement(int n) {
+        vector<int> visited(n+1,0);
         int res=0;
-        vector<bool> visited(n+1,false);
-        helper(n,visited,res,1);
+        helper(n,res,1,visited);
         return res;
     }
     
-    void helper(int n,vector<bool>& visited,int& res,int pos){
-        if(pos>n){
+    void helper(int n,int& res,int cur,vector<int>& visited){
+        if(cur>n){
             ++res;
             return;
         }
         for(int i=1;i<=n;++i){
-            if(visited[i]==false&&(i%pos==0||pos%i==0)){
-                visited[i]=true;
-                helper(n,visited,res,pos+1);
-                visited[i]=false;
+            if(visited[i]==0&&(i%cur==0||cur%i==0)){
+                visited[i]=1;
+                helper(n,res,cur+1,visited);
+                visited[i]=0;
             }
         }
     }
